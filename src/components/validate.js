b565178c489f7b6cd2acd95513f60653221b77f2
Kaitlyn Friden <kfriden@verizon.net>
2021-12-15 23:36:07 -0500

    edited validate file

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,19 +1,19 @@
-import Validator from "Validator";
-import isEmpty from 'lodash/isEmpty';
+// import Validator from "Validator";
+// import isEmpty from 'lodash/isEmpty';
 
-export default function validateInput(data) {
-    let errors = {};
+// export default function validateInput(data) {
+//     let errors = {};
 
-    if(Validator.isEmail(data.email)) {
-        errors.email = 'This field is required';
-    }
+//     if(Validator.isEmail(data.email)) {
+//         errors.email = 'This field is required';
+//     }
 
-    if(Validator.isStrongPassword(data.password)) {
-        errors.password = 'This field is required';
-    }
+//     if(Validator.isStrongPassword(data.password)) {
+//         errors.password = 'This field is required';
+//     }
 
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    };
-}
+//     return {
+//         errors,
+//         isValid: isEmpty(errors)
+//     };
+// }
